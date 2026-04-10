5f7c93dfe6af42cc78e888c75919fd24f038b6e2
Michael Valdemar <Valdemarmichael18@gmail.com>
2026-04-10 12:15:41 -0400

    Add files via upload

---
+++ Models/Miembro.cs
@@ -0,0 +1,10 @@
+namespace GymApp.Models
+{
+    public class Miembro
+    {
+        public int Id { get; set; }
+        public string NombreCompleto { get; set; }
+        public string Cedula { get; set; }
+        public string Telefono { get; set; }
+    }
+}
